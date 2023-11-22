eb5c799f20f0dd9a7222e2223e778cb71a6d446d
GamesReborn <gandalfverm@gmail.com>
2023-11-22 00:56:52 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://simple-proxy.gandalfverm.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTc3YTYyNGY1MzdhYTIzMjMwMTdhNGY5ZWZhZDBkMCIsInN1YiI6IjY1MzE5MzU5NmY4ZDk1MDBjOTI4NDRlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rpdD_cFHM5i8EzXwzdjFezfXvbDf1dXpKSvfBHN76C8"
+};
